c1b850beb0c522c3d86bfb4cb2644fbcc6511cd0
32193928 <32193928@dankook.ac.kr>
2025-11-05 15:31:31 +0900

    Vehicle추가

---
+++ Vehicle/TestVehicle.java
@@ -0,0 +1,10 @@
+package Vehicle;
+
+public class TestVehicle {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Scooter("Scooter_1");
+        System.out.println(vehicle.getId());
+
+
+    }
+}

f3e364a432288c8a5833b95a6838a717e840775c
32193928-hub <32193928@dankook.ac.kr>
2025-11-09 02:53:34 +0900

    Update Connect.java
    branch: final_1 
    file: rental/Connect.java
    
    JDBC_URL="...changtaejong.ddns.net:40040..."
    to change
    JDBC_URL="...129.154.48.223:3306..."

--- rental/Connect.java
+++ rental/Connect.java
@@ -2,7 +2,7 @@ package rental;
 
 public class Connect {
     public  final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    public  final String JDBC_URL = "jdbc:mysql://changtaejong.ddns.net:40040/dku?useAffectedRows=true";
+    public  final String JDBC_URL = "jdbc:mysql://129.154.48.223:3306/dku?useAffectedRows=true";
     public  final String DB_USER = "dku";
     public  final String DB_PASSWORD = "dkudku";
     public  final String TABLE_NAME = "vehicle";

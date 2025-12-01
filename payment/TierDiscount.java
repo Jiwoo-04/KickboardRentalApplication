1ad434b865b45112f3486a4bd551bc81d5cd6ba4
simjiseung <simjs050328@gmail.com>
2025-12-01 20:08:29 +0900

    발표용

--- payment/TierDiscount.java
+++ payment/TierDiscount.java
@@ -3,18 +3,17 @@ package payment;
 /**
  * 티어 할인(데코레이터)
  */
-public class TierDiscount implements Payment {
-    private final Payment payment;
+public class TierDiscount extends PaymentDecorator {
     private final double discountRate;
 
-    public TierDiscount(Payment payment, double discountRate) {
-        this.payment = payment;
+    public TierDiscount(Payment decoratedPayment, double discountRate) {
+        super(decoratedPayment);
         this.discountRate = discountRate;
     }
 
     @Override
     public double pay() {
-        double original = payment.pay(); // 정상 작동
+        double original = decoratedPayment.pay();
         double tier_discount = original * (1.0 - discountRate);
         System.out.println("티어할인 적용 (" + (discountRate * 100) + "% 할인): " + tier_discount + "원");
         return tier_discount;
